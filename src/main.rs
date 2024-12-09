fee063d29da314065931af289d67bab3110e3704
Hypercube8 <146479533+Hypercube8@users.noreply.github.com>
2024-12-09 00:50:09 +0000

    Normals render

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,26 @@ use ray::Ray;
 use vec3::{Vec3, Point3};
 use color::{Color, write_color};
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> Option<f64> {
     let oc = *center - r.origin;
     let a = Vec3::dot(r.direction, r.direction);
     let b = -2.0 * Vec3::dot(r.direction, oc);
     let c = Vec3::dot(oc, oc) - radius*radius;
     let discriminant = b*b - 4.0*a*c;
-    discriminant >= 0.0
+
+    if discriminant < 0.0 {
+        None
+    } else {
+        Some((-b - discriminant.sqrt()) / (2.0*a))
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, &r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let hit = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, &r);
+
+    if let Some(t) = hit {
+        let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit();
+        return 0.5 * Color::new(n.x+1.0, n.y+1.0, n.z+1.0);
     }
 
     let unit_direction = r.direction.unit();
